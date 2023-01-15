44e55ff7a7a7d2f9cbe056ac51bc6d4177f1ef4c
BenjiCCB <bensdecker+CCB@gmail.com>
2023-01-15 14:55:05 -0500

    pretty much done

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# challenge-09
-Challenge 09 Repo
+# Project ReadMe - nda 🏛️
+
+## Description 
+
+asdkln
+
+---
+
+## Table of Contents 
+
+* [Descripton](#description)
+
+* [Installation Instructions](#installation-instructions)
+
+* [Usage Information](#usage-information)
+
+* [App License](#app-license)
+
+* [Contribution Guidelines](#contribution-guidelines)
+
+* [Testing Instructions](#testing-instructions)
+
+* [Questions](#questions)
+
+
+
+---
+
+## Installation Instructions 
+
+asdlnka
+
+---
+
+## Usage Information 
+
+sadnl
+
+---
+
+## App License 
+
+This app is covered under the following license: MIT
+
+---
+
+## Contribution Guidelines 
+
+asd
+
+---
+
+## Testing Instructions 
+
+ads
+
+---
+
+## Questions 
+
+For more information and questions, please contact me at <bensdecker@gmail.com> or by visiting my [github account](https://github.com/benjiccb)
--- index.js
+++ index.js
@@ -5,26 +5,95 @@ inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'What is your app\'s title?',
+    },    
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is your app\'s description?',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      type: 'input',
+      name: 'installation',
+      message: 'What are your app\'s installation instructions?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is your app\'s usage information?',
     },
     {
       type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      message: 'What is your app\'s license type?',
+      name: 'license',
+      choices: ['MIT', 'Apache', 'Eclipse', 'Other'],
+    },
+    {
+      type: 'input',
+      name: 'guidelines',
+      message: 'What are your app\'s contribution guidelines?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are your app\'s testing instructions?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your contact email?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your Github account?',
     },
   ])
-  .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+  .then((data) => {    
+  
+    var TOCString = 
+    "* [Descripton](#description)\n\n" +
+    "* [Installation Instructions](#installation-instructions)\n\n" +
+    "* [Usage Information](#usage-information)\n\n" +
+    "* [App License](#app-license)\n\n" +
+    "* [Contribution Guidelines](#contribution-guidelines)\n\n" +
+    "* [Testing Instructions](#testing-instructions)\n\n" +
+    "* [Questions](#questions)\n\n"
+   
+    var badge;
+    if (data.license == "MIT"){
+      badge = "🏛️"
+    } else if (data.license == "Apache"){
+      badge = "📝"
+    } else if (data.license == "Eclipse"){
+      badge = "⭐"
+    }
+
+    // const filename = data.title + "-README.md";
+    const filename = "README.md";
+  
+    fs.writeFile(filename, 
+      '# Project ReadMe - ' + data.title + " " + badge + '\n\n' +
+      '## Description \n\n' +
+        data.description + '\n\n' + "---" + '\n\n' +
+      '## Table of Contents \n\n' +
+        TOCString + '\n\n' +  "---" + '\n\n' +
+      '## Installation Instructions \n\n' +
+        data.installation + '\n\n' + "---" + '\n\n' +
+      '## Usage Information \n\n' +
+        data.usage + '\n\n' + "---" + '\n\n' +
+      '## App License \n\n' +
+        'This app is covered under the following license: ' + data.license +'\n\n' + "---" + '\n\n' +
+      '## Contribution Guidelines \n\n' +
+        data.guidelines + '\n\n' + "---" + '\n\n' +
+      '## Testing Instructions \n\n' +
+        data.tests + '\n\n' + "---" + '\n\n' +
+      '## Questions \n\n' +
+        `For more information and questions, please contact me at <${data.email}> or by visiting my [github account](https://github.com/${data.github})`
+      ,
+    (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
